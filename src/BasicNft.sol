e1a984b19d11fdc9867d8520dc28e4385f9d5699
Sahib-Aujla <sahibpreet568@gmail.com>
2025-09-09 20:38:35 +0530

    added nft mint

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -4,5 +4,20 @@ pragma solidity ^0.8.19;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
-    constructor() ERC721("Kutta", "DOG") {}
+    uint256 private s_tokenCounter;
+    mapping(uint256=>string) private s_tokenIdToTokenUri;
+    constructor() ERC721("Kutta", "DOG") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft(string memory tokenUri) public returns(uint256){
+        s_tokenCounter++;
+        _safeMint(msg.sender,s_tokenCounter);
+        s_tokenIdToTokenUri[s_tokenCounter] = tokenUri;
+        return s_tokenCounter;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns(string memory){
+        return s_tokenIdToTokenUri[tokenId];
+    }
 }
