6d3d287644b8b5a604432390f6e575c61365c4fe
Sahib-Aujla <sahibpreet568@gmail.com>
2025-09-10 11:04:40 +0530

    added mint NFT on contract

---
+++ script/Interactions.s.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+
+contract MintBasicNft is Script {
+    string constant PUG =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function run() external {
+        address mostRecentDeployment = DevOpsTools.get_most_recent_deployment(
+            "BasicNft",
+            block.chainid
+        );
+        mintNftOnContract(mostRecentDeployment);
+    }
+
+    function mintNftOnContract(address basicNftAddress) public {
+        vm.startBroadcast();
+        BasicNft(basicNftAddress).mintNft(PUG);
+        vm.stopBroadcast();
+    }
+}
