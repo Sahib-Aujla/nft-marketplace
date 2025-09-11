ab09e224ea1680891c02d494b9839acc745ac0f9
Sahib-Aujla <sahibpreet568@gmail.com>
2025-09-11 09:20:26 +0530

    added mint NFT

--- src/MoonNft.sol
+++ src/MoonNft.sol
@@ -4,6 +4,10 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MoonNft is ERC721, Ownable {
+
+    event CreatedNFT(uint256 indexed tokenId, address indexed minter);
+
+
     uint256 private s_tokenCounter;
     string happyUri;
     string sadUri;
@@ -15,5 +19,23 @@ contract MoonNft is ERC721, Ownable {
         s_tokenCounter = 0;
         happyUri = _happyUri;
         sadUri = _sadUri;
+    }
+
+    function mintNft()public {
+        s_tokenCounter++;
+        _safeMint(msg.sender, s_tokenCounter);
+        emit CreatedNFT(s_tokenCounter, msg.sender);
+
+    }
+    function getHappySVG() public view returns (string memory) {
+        return happyUri;
+    }
+
+    function getSadSVG() public view returns (string memory) {
+        return sadUri;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
     }
 }
